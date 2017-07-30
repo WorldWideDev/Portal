14739d4f005e0af8850a10f22f4d9ade4f86be7e
WorldWideDev <dev206@gmail.com>
2017-07-29 18:10:43 -0700

    fixing some debug code

--- apps/course_sessions/urls.py
+++ apps/course_sessions/urls.py
@@ -2,10 +2,10 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    # url(r'^$', views.index, name='index'),
-    url(r'^home$', views.sup, name='home'),
-    # url(r'^(?P<co_id>\d+)$', views.index, name='index_pattern'),
-    # url(r'^(?P<co_id>\d+)/(?P<filter_kw>\w+)$', views.index, name='index_filter'),
-    # url(r'^sesh/(?P<pk>\d+)$', views.SessionDetailView.as_view(), name='details'),
-    # url(r'^update_session$', views.update_session, name='update_session')
+    url(r'^$', views.index, name='index'),
+    url(r'^home$', views.home_redirect, name='home'),
+    url(r'^(?P<co_id>\d+)$', views.index, name='index_pattern'),
+    url(r'^(?P<co_id>\d+)/(?P<filter_kw>\w+)$', views.index, name='index_filter'),
+    url(r'^sesh/(?P<pk>\d+)$', views.SessionDetailView.as_view(), name='details'),
+    url(r'^update_session$', views.update_session, name='update_session')
 ]
--- apps/course_sessions/views.py
+++ apps/course_sessions/views.py
@@ -13,51 +13,48 @@ from django.apps.registry import apps
 
 MAIN_TEMPLATE = 'course_sessions/index.html'
 
-def sup(req):
-    return HttpResponse("sup")
+@login_required
+def home_redirect(req):
+    sesh = Session.objects.users_current_session_or_none(req.user.id, Cohort.objects.current_cohort_id())
+    if sesh:
+        return HttpResponseRedirect(reverse("sessions:details", kwargs={"pk": sesh.id}))
+    return HttpResponseRedirect(reverse("sessions:index"))
 
-# @login_required
-# def home_redirect(req):
-#     sesh = Session.objects.users_current_session_or_none(req.user.id, Cohort.objects.current_cohort_id())
-#     if sesh:
-#         return HttpResponseRedirect(reverse("sessions:details", kwargs={"pk": sesh.id}))
-#     return HttpResponseRedirect(reverse("sessions:index"))
-
-# @login_required
-# # this view will default to show sessions for the 'current' cohort
-# def index(req, co_id=Cohort.objects.current_cohort_id(), filter_kw="unassigned"):
-#     try:
-#         prev_id = Cohort.objects.previous_cohort(co_id).id
-#     except:
-#         prev_id = None
-#     try:
-#         next_id = Cohort.objects.next_cohort(co_id).id
-#     except:
-#         next_id = None
+@login_required
+# this view will default to show sessions for the 'current' cohort
+def index(req, co_id=Cohort.objects.current_cohort_id(), filter_kw="unassigned"):
+    try:
+        prev_id = Cohort.objects.previous_cohort(co_id).id
+    except:
+        prev_id = None
+    try:
+        next_id = Cohort.objects.next_cohort(co_id).id
+    except:
+        next_id = None
     
-#     context = {
-#         # "students": Student.objects.assignment_filter(filter_kw, co_id),
-#         "session_start": Cohort.objects.get(id=co_id),
-#         "sessions": Session.objects.filter(start_date_id=co_id),
-#         "prev_id": prev_id,
-#         "next_id": next_id,
-#         "ass_filter": ["unassigned", "assigned"]
-#     }
-#     return render(req, MAIN_TEMPLATE, context)
+    context = {
+        # "students": Student.objects.assignment_filter(filter_kw, co_id),
+        "session_start": Cohort.objects.get(id=co_id),
+        "sessions": Session.objects.filter(start_date_id=co_id),
+        "prev_id": prev_id,
+        "next_id": next_id,
+        "ass_filter": ["unassigned", "assigned"]
+    }
+    return render(req, MAIN_TEMPLATE, context)
 
-# class SessionDetailView(LoginRequiredMixin, DetailView):
-#     model = Session
+class SessionDetailView(LoginRequiredMixin, DetailView):
+    model = Session
 
-# def update_session(req):
-#     try: 
-#         req.POST.getlist('to_assign')
-#         Student.objects.add_to_session(req.POST)
-#     except KeyError:
-#         messages.error(req, "Please select a student to add")
-#         context = {
-#             "students": Student.objects.filter(status='active'),
-#             "start_dates": Cohort.objects.all(),
-#             "courses": Course.objects.all()
-#         }
-#         return render(req, MAIN_TEMPLATE, context)
-#     return HttpResponseRedirect(reverse("sessions:index"))
+def update_session(req):
+    try: 
+        req.POST.getlist('to_assign')
+        Student.objects.add_to_session(req.POST)
+    except KeyError:
+        messages.error(req, "Please select a student to add")
+        context = {
+            "students": Student.objects.filter(status='active'),
+            "start_dates": Cohort.objects.all(),
+            "courses": Course.objects.all()
+        }
+        return render(req, MAIN_TEMPLATE, context)
+    return HttpResponseRedirect(reverse("sessions:index"))
